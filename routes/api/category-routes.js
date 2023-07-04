cf8b95fc70a48a227e06668c4cdfdd9e4cd00c5c
Thomas Henderson <trhenderson303@gmail.com>
2023-07-04 14:17:13 -0600

    debugged route to get tag by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,7 +31,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
-    const categoryData = await Category.create(req.body.id);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,19 +19,19 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }, { model: ProductTag }]
+      include: [{ model: Product }]
     });
     if (!tagData) {
-      res.status(404).json({ message: 'Tag not found' });
+      res.status(400).json({ message: 'Tag not found' });
       return;
+    } else {
+      res.status(200).json(tagData);
     }
-    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
 });
+
 
 router.post('/', async (req, res) => {
   try {
